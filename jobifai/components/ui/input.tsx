4e6cc0c3e4d054ef50e60927ec770c1e75986338
Cristian Leo <cristianleo120@gmail.com>
2024-10-04 18:54:28 -0400

    fix deployment issues

--- jobifai/components/ui/input.tsx
+++ jobifai/components/ui/input.tsx
@@ -2,7 +2,8 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   // Add a placeholder member to avoid the lint error
   placeholder?: string;
 }
